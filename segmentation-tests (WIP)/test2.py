25114366705cdff5bb587ca1e0f061f5066109e2
Jonathan David Avendaño Ortega <jdavidavendano@gmail.com>
2016-10-24 23:15:33 -0500

    Superpixels as graphs

--- segmentation-tests (WIP)/test2.py
+++ segmentation-tests (WIP)/test2.py
@@ -4,6 +4,8 @@ from skimage.color import rgb2gray
 from skimage.filters import threshold_otsu
 from skimage.segmentation import slic, mark_boundaries
 from matplotlib.pyplot import show, imshow, subplot
+from skimage.future import graph
+from matplotlib import colors
 
 I = imread('imgs/IMD242.bmp')
 GT = rgb2gray(imread('imgs/IMD242_lesion.bmp').astype(float)) > 120
@@ -11,10 +13,17 @@ GT = rgb2gray(imread('imgs/IMD242_lesion.bmp').astype(float)) > 120
 # Try other numbers
 L = slic(I, n_segments=900)
 
-subplot(1,2,1)
-imshow(mark_boundaries(I, L))
+subplot(1, 2, 1)
+Islic = mark_boundaries(I, L)
+imshow(Islic)
 
-subplot(1,2,2)
+subplot(1, 2, 2)
 imshow(mark_boundaries(GT, L, color=(1, 0, 0)))
 
+show()
+
+g = graph.rag_mean_color(I, L)
+lc = graph.draw_rag(L, g, Islic)
+
+imshow(lc)
 show()
