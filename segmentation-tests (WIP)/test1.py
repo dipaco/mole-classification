f63843e9534f283ecfc2f05d426c2353870a958a
Jonathan David Avendaño Ortega <jdavidavendano@gmail.com>
2016-10-24 21:55:33 -0500

    Step 5

--- segmentation-tests (WIP)/test1.py
+++ segmentation-tests (WIP)/test1.py
@@ -3,15 +3,20 @@ Requeriments:
 - Python (We use 3.5).
 - Packages: scipy, matplotlib, cv2, skimage, numpy.
 '''
+
 from scipy.ndimage import imread
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow, show, title, figure, subplot, suptitle
 import cv2 as cv
 from skimage.filters import threshold_otsu
 from skimage import transform
-from skimage.morphology import erosion, dilation, disk, square, diamond, closing
+from skimage.morphology import erosion, dilation, disk, square, diamond, closing, black_tophat, white_tophat, opening
 from skimage.measure import label
 import numpy as np
 from skimage.draw import ellipse
+from scipy.io import loadmat
+from skimage.segmentation import slic, mark_boundaries
+from skimage.measure import compare_nrmse
+from skimage import img_as_ubyte
 
 def rgb2gray(rgb):
     gray = 0.2989 * rgb[..., 0]
@@ -60,18 +65,43 @@ skinMoleB = maskCorners < IOtsu
 '''
 
 # Method 3: ellipse
-mask1 = np.zeros((I.shape[0], I.shape[1]), dtype=np.uint8)
-rr, cc = ellipse(round(I.shape[0]/2), round(I.shape[1]/2), round((I.shape[0]/2)*(11/12)), round((I.shape[1]/2)*(11/12)))
+mask1 = np.zeros((I.shape[0:2]), dtype=np.uint8)
+rr, cc = ellipse(round(I.shape[0]/2), round(I.shape[1]/2), round((I.shape[0]/2)*(11/12.0)), round((I.shape[1]/2)*(11/12.0)))
 mask1[rr, cc] = 1
 
 skinMoleC = mask1 * IOtsu
-# plt.imshow(skinMoleC, cmap="gray")
+# imshow(skinMoleC, cmap="gray")
 
-'''
 # Step 5
+'''
 # Temporal disk, it was not successful
 disk11 = disk(11)
 skinMoleClosing = closing(skinMoleC, selem=disk11)
 '''
+# Disks created by radial decomposition using periodic lines
+data = loadmat('se.mat')
 
-plt.show()
+skinMoleClosing0 = closing(skinMoleC, selem=data['se']['se0'][0][0])
+skinMoleClosing4 = closing(skinMoleC, selem=data['se']['se4'][0][0])
+skinMoleClosing6 = closing(skinMoleC, selem=data['se']['se6'][0][0])
+
+skinMoleOpening0 = opening(skinMoleClosing0, selem=data['se']['se0'][0][0])
+skinMoleOpening4 = opening(skinMoleClosing4, selem=data['se']['se4'][0][0])
+skinMoleOpening6 = opening(skinMoleClosing6, selem=data['se']['se6'][0][0])
+
+# Ground truth
+IS = imread('imgs/IMD242_lesion.bmp')
+
+figure(0)
+suptitle('Our - Diff - Ground truth')
+
+subplot(1, 3, 1)
+imshow(skinMoleOpening0, cmap='gray')
+
+subplot(1, 3, 2)
+imshow(255 - ((255 - (IS * skinMoleOpening0)) + (IS == skinMoleOpening0)), cmap='gray')
+
+subplot(1, 3, 3)
+imshow(IS, cmap='gray')
+
+show()
