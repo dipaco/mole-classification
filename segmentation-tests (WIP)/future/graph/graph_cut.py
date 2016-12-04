b3f12e4d9ac0524b409e895e376c20e8b320b383
Jonathan David Avendaño Ortega <jdavidavendano@gmail.com>
2016-12-04 15:34:54 -0500

    bug fix

--- segmentation-tests (WIP)/future/graph/graph_cut.py
+++ segmentation-tests (WIP)/future/graph/graph_cut.py
@@ -5,7 +5,7 @@ except ImportError:
     warnings.warn('RAGs require networkx')
 import numpy as np
 from . import _ncut
-from . import _ncut_cy
+from skimage.future.graph import _ncut_cy
 from scipy.sparse import linalg
 
 
